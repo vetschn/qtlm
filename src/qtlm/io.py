3d0b46378dd66ee380c2b7a51230ee0b73a1c1e7
Nicolas Vetsch <vetschn@iis.ee.ethz.ch>
2025-07-26 16:51:36 +0200

    No more k-dependent data, tight-binding like format instead.

--- src/qtlm/io.py
+++ src/qtlm/io.py
@@ -4,21 +4,18 @@ from mpi4py.MPI import COMM_WORLD as comm
 from mpi4py.util import pkl5
 
 from qtlm import NDArray, xp
+import numpy as np
 
 comm = pkl5.Intracomm(comm)
 
 
-def read_gpaw_hamiltonian(directory: str | Path) -> NDArray:
-    """Reads a GPAW Hamiltonian from a file.
-
-    This autodetects whether the file is in npy or sparse npz format.
-    Only the root rank reads the file, and the data is broadcasted to
-    all ranks.
+def read_tight_binding_data(directory: str | Path) -> NDArray:
+    """Reads the tight-binding data from the specified directory.
 
     Parameters
     ----------
     directory : str
-        Directory containing the GPAW Hamiltonian file.
+        Path to the directory containing the tight-binding data files.
 
     Returns
     -------
@@ -26,25 +23,39 @@ def read_gpaw_hamiltonian(directory: str | Path) -> NDArray:
         The Hamiltonian matrix.
     NDArray
         The overlap matrix.
+    NDArray
+        The lattice vectors.
 
     """
-    H_kMM = None
-    S_kMM = None
-    if comm.rank == 0:
-        hamiltonian_filename = Path(directory) / "H_kMM.npy"
-        overlap_filename = Path(directory) / "S_kMM.npy"
-        if not hamiltonian_filename.exists():
-            raise FileNotFoundError(f"File {hamiltonian_filename} not found.")
-        if not overlap_filename.exists():
-            raise FileNotFoundError(f"File {overlap_filename} not found.")
-        if hamiltonian_filename.suffix != ".npy":
-            raise NotImplementedError()
-        if overlap_filename.suffix != ".npy":
-            raise NotImplementedError()
-        H_kMM = xp.load(hamiltonian_filename)[0]
-        S_kMM = xp.load(overlap_filename)
+    hamiltonian_filename = Path(directory) / "hamiltonian_r.npy"
+    overlap_filename = Path(directory) / "overlap_r.npy"
+    lattice_vectors_filename = Path(directory) / "r.dat"
 
-    H_kMM = comm.bcast(H_kMM, root=0)
-    S_kMM = comm.bcast(S_kMM, root=0)
+    if not hamiltonian_filename.exists():
+        raise FileNotFoundError(f"File {hamiltonian_filename} not found.")
+    if not overlap_filename.exists():
+        raise FileNotFoundError(f"File {overlap_filename} not found.")
+    if not lattice_vectors_filename.exists():
+        raise FileNotFoundError(f"File {lattice_vectors_filename} not found.")
 
-    return H_kMM, S_kMM
+    # Only
+    h_r = xp.load(hamiltonian_filename)
+    print(
+        f"Rank {comm.rank} read {h_r.shape} tight-binding Hamiltonian.",
+        flush=True,
+    )
+
+    s_r = xp.load(overlap_filename)
+    print(
+        f"Rank {comm.rank} read {s_r.shape} overlap.",
+        flush=True,
+    )
+    comm.barrier()
+
+    lattice_vectors = xp.loadtxt(lattice_vectors_filename)
+    print(
+        f"Rank {comm.rank} read lattice vectors: {lattice_vectors.shape}.",
+        flush=True,
+    )
+
+    return h_r, s_r, lattice_vectors
