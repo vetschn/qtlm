85b7dfec84e4705851ab3e5bfd35fc80f8a9d1f6
Nicolas Vetsch <vetschn@iis.ee.ethz.ch>
2025-07-26 16:51:13 +0200

    Updated constants

--- src/qtlm/constants.py
+++ src/qtlm/constants.py
@@ -1,5 +1,6 @@
 from scipy.constants import physical_constants
 
-k_B = physical_constants["Boltzmann constant in eV/K"][0]  # eV / K
 e = physical_constants["elementary charge"][0]  # C
-hbar = physical_constants["reduced Planck constant in eV s"][0]  # eV s
+epsilon_0 = physical_constants["electric constant"][0]  # F/m
+hbar = physical_constants["reduced Planck constant"][0]  # J s
+k_B = physical_constants["Boltzmann constant in eV/K"][0]  # eV / K
