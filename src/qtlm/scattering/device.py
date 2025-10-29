c4c73a5cfc2c24fec051c7c0667d257713dfa61f
deborah-cht <deborah.chaouat@gmail.com>
2025-10-29 15:43:54 +0100

    'add polarisation, selfenergy, green function d

--- src/qtlm/scattering/device.py
+++ src/qtlm/scattering/device.py
@@ -1,3 +1,9 @@
+from qtlm.constants import e, hbar, c_0, eV_to_J
+from qtlm import NDArray, xp
+import opt_einsum as oe
+import scipy.sparse as sp
+eV_to_J = 1.602176634e-19  # Conversion factor from eV to Joules
+
 class Device:
     
     def __init__(self):
@@ -6,12 +12,99 @@ class Device:
         self.interaction_tensor = None
         self.positions = None
         self.distances = None
-        
-    def compute_d0(self):
-        ...
 
-    def compute_d0_delta_perp(self):
-        ...
+
+        self.prefactor = (-e / 2.0) * (1j / hbar * eV_to_J)  # in SI units (C * s / J)
+
+        self.interaction_tensor  = self.prefactor * self.hamiltonian.toarray()[..., xp.newaxis] * self.distances  # CPU
+
+    # M = xp.ndarray(comp.astype(complex), dtype=complex)
+
+        
+    def compute_d0(self,photon_energies):
+        """
+        3D tensor D0[m, i, j] of Initial Photon Green's functions between each pair (n,m) of positions
+        R_positions : (N,3) array of positions of orbitals
+        E_eV_array : (M,) array of energies in eV ((invented))
+        """
+        omega = photon_energies / (hbar * eV_to_J)  # angular frequencies in rad/s
+        k = omega / c_0
+
+        r = xp.linalg.norm(self.distances, axis=2)  # (N, N)
+        r_norm = r.copy()
+        xp.fill_diagonal(r_norm, xp.inf)
+        
+        D0 = xp.exp(1j * k[:, None, None] * r_norm[None, :, :]) / (
+            4 * xp.pi * r_norm[None, :, :]
+        )
+        # # Set diagonal to zero (Do we exclude self-interaction here?)
+        # for m in range(D0.shape[0]):
+        #     xp.fill_diagonal(D0[m], 0.0)
+
+        return D0  # shape (Nw,N,N)
+
+    def compute_d0_delta_perp(self, photon_energies):
+        
+        D0 = self.compute_d0(self, photon_energies)
+        sigma=1e-10, 
+        tol=0.0
+        N = self.distances.shape[0]
+        pref = 1.0 / (4.0 * xp.pi)
+
+        # self.distances
+        r_mn_2 = xp.sum(self.distances**2, axis=2)
+        r_mn = xp.sqrt(r_mn_2)
+
+        mark = r_mn > 0
+
+        # δ^{(3)}(r) ~ gaussienne 3D
+        norm = (2.0 * sigma**2 * xp.pi) ** (-3 / 2)
+        delta_3D = norm * xp.exp(-r_mn_2 / (2.0 * sigma**2))  # (N,N)
+
+        # Hessian Matrix of 1/r for r!=0 : ∂i∂j(1/r) = (3 r_i r_j - r^2 δ_ij)/r^5
+        inv_r5 = xp.zeros_like(r_mn)
+        inv_r5[mark] = 1.0 / (r_mn[mark] ** 5)
+
+        delta_transverse = {}
+        for i in range(3):
+            ri = self.distances[..., i]  # (N,N)
+            for j in range(3):
+                rj = self.distances[..., j]
+                delta_ij = 1.0 if i == j else 0.0
+
+                # δ⊥_{ij}} = δ_ab δ^{(3)}(r)  +  pref * [ 3 r_a r_b / r^5  - δ_ab * r^2 / r^5 ]
+                delta_transversal = delta_ij * delta_3D + pref * (
+                    3.0 * ri * rj * inv_r5 - delta_ij * r_mn_2 * inv_r5
+                )
+
+                if tol > 0.0:
+                    keep = xp.abs(delta_transversal) > tol
+                    rows, cols = xp.nonzero(keep)
+                    data = delta_transversal[keep]
+                    delta_transverse[(i, j)] = sp.coo_matrix(
+                        (data, (rows, cols)), shape=(N, N)
+                    ).tocsr()
+                else:
+                    delta_transverse[(i, j)] = sp.csr_matrix(delta_transversal)
+            
+            
+        num_orbital = self.distances.shape[0]
+
+        # stack into dense tensor Delta[i,j,u,v]
+        Delta = xp.empty(
+            (num_orbital, num_orbital, 3, 3), dtype=float
+        )  # or complex if needed
+        for u in range(3):
+            for v in range(3):
+                D_t = delta_transverse[(u, v)]
+                Delta[:, :, u, v] = D_t.toarray() if sp.issparse(D_t) else xp.asarray(D_t)
+
+        out = oe.contract("wij,jkuv->wikuv", D0, Delta)
+
+        return out  # (Nw, N, N, 3, 3)
+
 
     def configure():
         ...
+    
+
--- src/qtlm/scattering/self_energy.py
+++ src/qtlm/scattering/self_energy.py
@@ -1,4 +1,181 @@
+import time
+
+import opt_einsum as oe
+import scipy
+
+
+from qtlm import NDArray, xp
+from qtlm.scattering.device import Device
+
+from qtlm.constants import mu_0
+from qtlm.config import QTLMConfig
+from qtlm.io import read_tight_binding_data
 class SelfEnergy:
+    """Photon self-energy within the self-consistent Born approximation (SCBA).
+
+    Attributes:
+      compute_config:  ComputeConfig
+      qc:              QuatrexConfig
+      m_interaction:   (N, N, 3)   real/complex, energy-independent
+    """
+
+    def __init__(
+        self,
+        qtlm_config: QTLMConfig,
+        energies: NDArray,
+        photon_energies: NDArray,
+        sparsity_pattern: sparse.coo_matrix = None,
+    ) -> None:
+
+        # grids
+        self.energies = energies
+        self.photon_energies = photon_energies
+        self.Ne = energies.size
+        self.Nw = photon_energies.size
+
+        # constants
+        self.prefactor = 1j * mu_0 * (1 / (2 * xp.pi))
+        self.dE = xp.diff(energies).mean()
+        self.dhw = xp.diff(photon_energies).mean()
+
+        # --- configuration extraction ---
+
+        # LOAD Hamiltonian (same as in electron transport)
+       
+
+
+        # LOAD distances
+
     
-    def __init__(self):
-        pass
+
+        # System matrix storage with photon stack shape (energies)
+        self.system_matrix
+
+        self.system_matrix.free_data()
+        del sparsity_pattern
+
+
+    def compute(
+        self,
+        g_lesser: NDArray,
+        d_lesser: NDArray,
+        out: tuple[NDArray, NDArray, NDArray],
+    ) -> None:
+        """Compute the photon self-energy Σ.
+
+        Args:
+          g:        (Ne, N, N) complex
+          d:        (N, N) complex
+          outputs:  tuple of NDArray to store results
+                    each of shape (Nw, N, N, 3, 3) complex
+        """
+
+        s_lesser, s_greater, s_retarded = out
+
+        #Guard 
+        if self.overlap_sparray is None:
+            self.overlap_sparray = sparse.eye(
+            self.hamiltonian.shape[-2],
+            format="coo",
+            dtype=self.hamiltonian.dtype,
+        )
+        # compute self-energy
+
+        if not xp.allclose(xp.diff(self.energies), self.dE, rtol=1e-6, atol=1e-12):
+            raise ValueError("energy_grid should be uniformly spaced for FFT")
+
+        if not xp.allclose(xp.diff(self.energies), self.dhw, rtol=1e-6, atol=1e-12):
+            raise ValueError("photon_energy should be uniformly spaced for FFT")
+
+        if not xp.isclose(self.dhw, self.dE):
+            raise ValueError(
+                f"Mismatch in spacing : Δω={self.dhw:.3e} vs ΔEs={self.dE:.3e}"
+            )
+
+        n = self.Nw + self.Nw - 1  # padding
+        start_ifft_timer = time.perf_counter()
+        # FFT: energy/frequency domain to time domain: energy -> tau
+        G_IFFT = scipy.fft.fft(
+            g_lesser, n, axis=0, workers=128
+        )  # (Np, N, N) #TODO: change to the fastest option
+        G_IFFT = xp.flip(G_IFFT, axis=0)  # reverse the order to get G(tau)
+        # G_IFFT = xp.conj(G_IFFT[::-1, ...])  # reverse the order to get G(tau)
+        D_IFFT = scipy.fft.fft(d_lesser, n, axis=0, workers=128)  # (Np, N, N)
+        end_ifft_timer = time.perf_counter()
+        print(
+            f"first fourier transform took {end_ifft_timer - start_ifft_timer:.3f}s"
+        )  # np : 27.7 sec | scipy : 0.989s
+
+        # Get the term for the transverse self-energy
+        indices_list = [
+            "iju,til,lkv,tikuv->tjk",
+            "iju,til,lkv,tiluv->tjk",  # optimized scaling at 6
+            "iju,til,lkv,tjkuv->tjk",  # optimized scaling at 6
+            "iju,til,lkv,tjluv->tjk",
+        ]
+        SUM = None
+        for i in indices_list:
+
+            start = time.perf_counter()
+            path, path_info = oe.contract_path(
+                i,
+                self.m_interaction,
+                G_IFFT,
+                self.m_interaction,
+                D_IFFT,
+                optimize="optimal",
+                memory_limit="max_input",
+            )
+            end = time.perf_counter()
+            print(
+                path_info,
+            )  # optionnel: affiche le plan de contraction
+            print(end - start)
+
+            Term = oe.contract(
+                i,
+                self.m_interaction,
+                G_IFFT,
+                self.m_interaction,
+                D_IFFT,
+                optimize=path,
+                memory_limit="max_input",
+            )
+            # later passes: mutate in place
+            if SUM is None:
+                # first pass: take a writable copy, do NOT add twice
+                SUM = Term + 0
+            else:
+                SUM += Term
+
+            del Term
+
+        print("Be patient, FFT back is starting...")
+
+        time_FFT_start = time.perf_counter()
+        Sigma_full = xp.fft.ifft(SUM, axis=0)  # (n, N, N, 3, 3)
+        Sigma_full = self.prefactor * Sigma_full
+        time_FFT_end = time.perf_counter()
+        print(
+            f"back fourier transform took {time_FFT_end - time_FFT_start:.3f}s"
+        )  # in np : 0.583s | scipy : 0.149s
+
+        # index array
+        idx = xp.round((self.energies - self.energies[0]) / self.dhw).astype(int)
+
+        if xp.any((idx < 0) | (idx >= Sigma_full.shape[0])):
+
+            bad = self.photon_energies[(idx < 0) | (idx >= Sigma_full.shape[0])]
+            raise ValueError(f"Some requeste energies fall outside the FFT grid: {bad}")
+
+        # select only selected electron energies and corresponding polarization values
+        sigma_selected = Sigma_full[idx, ...]  # (NE, N, N, 3, 3)
+
+        s_lesser.nonzero()
+
+        s_lesser[...] += sigma_selected
+        s_greater[...] += xp.conj(s_lesser[::-1].transpose(0, 2, 1))
+        # s_greater[...] = -xp.conj(s_lesser.transpose(0, 2, 1, 4, 3)) #fermionic nature
+        s_retarded[...] += 0.5 * (s_lesser - s_greater)
+
+
